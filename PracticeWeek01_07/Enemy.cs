d88d3f95e957657f52a6d7a5072d110b59d16f8d
MoodyShoo <dimacold22@gmail.com>
2024-02-21 18:15:29 +0300

    Added Task 7 (I hate this console labirynth)

---
+++ PracticeWeek01_07/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_07
+{
+    class Enemy
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private Random random = new Random();
+
+        public Enemy(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void MoveRandomly(char[,] map)
+        {
+            int dx = random.Next(-1, 2); // Генерируем случайное значение от -1 до 1 для движения по оси X
+            int dy = random.Next(-1, 2); // Генерируем случайное значение от -1 до 1 для движения по оси Y
+
+            int newX = X + dx;
+            int newY = Y + dy;
+
+            // Проверяем, что новые координаты находятся в пределах карты и являются допустимыми
+            if (newX >= 0 && newX < map.GetLength(0) && newY >= 0 && newY < map.GetLength(1) && map[newX, newY] == ' ')
+            {
+                X = newX;
+                Y = newY;
+            }
+        }
+    }
+}
