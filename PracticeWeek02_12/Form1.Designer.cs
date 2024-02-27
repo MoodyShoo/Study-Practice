45e634deb8248d1cbfcabab3ad9a4971d40fe722
MoodyShoo <dimacold22@gmail.com>
2024-02-28 00:18:30 +0300

    Completed Task 12

--- PracticeWeek02_12/Form1.Designer.cs
+++ PracticeWeek02_12/Form1.Designer.cs
@@ -30,15 +30,16 @@
         {
             this.newGameButton = new System.Windows.Forms.Button();
             this.panelSudoku = new System.Windows.Forms.Panel();
+            this.checkButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // newGameButton
             // 
-            this.newGameButton.Location = new System.Drawing.Point(550, 12);
+            this.newGameButton.Location = new System.Drawing.Point(471, 12);
             this.newGameButton.Name = "newGameButton";
-            this.newGameButton.Size = new System.Drawing.Size(113, 43);
+            this.newGameButton.Size = new System.Drawing.Size(113, 57);
             this.newGameButton.TabIndex = 0;
-            this.newGameButton.Text = "button1";
+            this.newGameButton.Text = "Новая игра";
             this.newGameButton.UseVisualStyleBackColor = true;
             this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
             // 
@@ -49,11 +50,22 @@
             this.panelSudoku.Size = new System.Drawing.Size(400, 400);
             this.panelSudoku.TabIndex = 1;
             // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(471, 75);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(113, 61);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Проверить";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 420);
+            this.ClientSize = new System.Drawing.Size(673, 420);
+            this.Controls.Add(this.checkButton);
             this.Controls.Add(this.panelSudoku);
             this.Controls.Add(this.newGameButton);
             this.Name = "Form1";
@@ -66,6 +78,7 @@
 
         private System.Windows.Forms.Button newGameButton;
         private System.Windows.Forms.Panel panelSudoku;
+        private System.Windows.Forms.Button checkButton;
     }
 }
 
--- PracticeWeek02_12/Form1.cs
+++ PracticeWeek02_12/Form1.cs
@@ -25,6 +25,150 @@ namespace PracticeWeek02_12
         private void newGameButton_Click(object sender, EventArgs e)
         {
             startNewGame();
+            UnhighlightAllCells();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            HighlightDuplicates();
+        }
+
+        private void HighlightDuplicates()
+        {
+            List<int> rowsWithDuplicates = new List<int>();
+            List<int> columnsWithDuplicates = new List<int>();
+            List<Tuple<int, int>> squaresWithDuplicates = new List<Tuple<int, int>>();
+
+            // Проверка строк
+            for (int i = 0; i < 9; i++)
+            {
+                List<int> valuesInRow = new List<int>();
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!string.IsNullOrEmpty(cells[i, j].Text) && int.TryParse(cells[i, j].Text, out int value))
+                    {
+                        if (valuesInRow.Contains(value))
+                        {
+                            rowsWithDuplicates.Add(i);
+                            break;
+                        }
+                        else
+                        {
+                            valuesInRow.Add(value);
+                        }
+                    }
+                }
+            }
+
+            // Проверка столбцов
+            for (int j = 0; j < 9; j++)
+            {
+                List<int> valuesInColumn = new List<int>();
+                for (int i = 0; i < 9; i++)
+                {
+                    if (!string.IsNullOrEmpty(cells[i, j].Text) && int.TryParse(cells[i, j].Text, out int value))
+                    {
+                        if (valuesInColumn.Contains(value))
+                        {
+                            columnsWithDuplicates.Add(j);
+                            break;
+                        }
+                        else
+                        {
+                            valuesInColumn.Add(value);
+                        }
+                    }
+                }
+            }
+
+            // Проверка квадратов 3 на 3
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3)
+                {
+                    List<int> valuesInSquare = new List<int>();
+                    for (int x = i; x < i + 3; x++)
+                    {
+                        for (int y = j; y < j + 3; y++)
+                        {
+                            if (!string.IsNullOrEmpty(cells[x, y].Text) && int.TryParse(cells[x, y].Text, out int value))
+                            {
+                                if (valuesInSquare.Contains(value))
+                                {
+                                    squaresWithDuplicates.Add(new Tuple<int, int>(i, j));
+                                    break;
+                                }
+                                else
+                                {
+                                    valuesInSquare.Add(value);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            // Подсветка всех строк с дубликатами
+            foreach (int row in rowsWithDuplicates)
+            {
+                HighlightRow(row);
+            }
+
+            // Подсветка всех столбцов с дубликатами
+            foreach (int col in columnsWithDuplicates)
+            {
+                HighlightColumn(col);
+            }
+
+            // Подсветка всех квадратов с дубликатами
+            foreach (var square in squaresWithDuplicates)
+            {
+                HighlightSquare(square.Item1, square.Item2);
+            }
+
+            // Снятие подсветки, если дубликатов нет
+            if (rowsWithDuplicates.Count == 0 && columnsWithDuplicates.Count == 0 && squaresWithDuplicates.Count == 0)
+            {
+                UnhighlightAllCells();
+            }
+        }
+        private void HighlightRow(int row)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cells[row, j].BackColor = Color.Red;
+            }
+        }
+
+        private void HighlightColumn(int col)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                cells[i, col].BackColor = Color.Red;
+            }
+        }
+
+        private void HighlightSquare(int startX, int startY)
+        {
+            for (int i = startX; i < startX + 3; i++)
+            {
+                for (int j = startY; j < startY + 3; j++)
+                {
+                    cells[i, j].BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void UnhighlightAllCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                }
+            }
         }
 
         private void startNewGame()
@@ -152,7 +296,6 @@ namespace PracticeWeek02_12
                 cell.ForeColor = SystemColors.ControlDarkDark;
             }
         }
-
 
         private void SaveCellValuesToFile()
         {
