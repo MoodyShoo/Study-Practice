8b07fd4009044c1cff4fefab4229d98931ea4683
MoodyShoo <dimacold22@gmail.com>
2024-03-01 17:09:37 +0300

    Added Task 15 classes

---
+++ PracticeWeek02_15/Fishes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeWeek02_15
+{
+    public partial class Fishes : Form
+    {
+        class TFish
+        {
+            private int _x;
+            private int _y;
+            private double _speed;
+            private string _color;
+            private string _direction;
+
+            public void Init()
+            {
+
+            }
+
+            public virtual void Draw()
+            {
+
+            }
+
+            private void Look(){
+
+            }
+            public void Run()
+            {
+
+            }
+        }
+
+        class Tpike : TFish
+        {
+            public override void Draw()
+            {
+
+            }
+        }
+
+        class Tkarp : TFish
+        {
+            public override void Draw()
+            {
+
+            }
+        }
+
+        class Taquarium
+        {
+            public void Init()
+            {
+
+            }
+
+            public void Run()
+            {
+
+            }
+
+            public void Done()
+            {
+
+            }
+        }
+
+        public Fishes()
+        {
+            InitializeComponent();
+        }
+    }
+}
